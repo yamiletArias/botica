657ef05156bb48e35fd48de0ca73388bb1f5facc
Yamilet <deya.meme.08@gmail.com>
2025-05-10 18:49:23 -0500

    TERMINADO UWU

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 
 import express from 'express'
-import softwaresRouters from './routes/softwares.routes.js'
+import medicamentosRouters from './routes/softwares.routes.js'
 
 const app = express()
 
 app.use(express.json()) 
-app.use('/api/', softwaresRouters);
+app.use('/api/', medicamentosRouters);
 
 export default app
 
